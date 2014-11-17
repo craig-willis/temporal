edaedf3da398b9b32715a75a173970859fdadeba
craig-willis <willis8@illinois.edu>
2014-11-17 14:50:15 -0600

    Added temporalscorer interface

---
+++ src/main/java/edu/gslis/temporal/scorers/TemporalScorer.java
@@ -0,0 +1,16 @@
+package edu.gslis.temporal.scorers;
+
+import java.text.DateFormat;
+
+import edu.gslis.docscoring.QueryDocScorer;
+
+public abstract class TemporalScorer extends QueryDocScorer {
+
+    public abstract void setStartTime(long startTime);
+    public abstract void setEndTime(long endTime);
+    public abstract void setInterval(long interval);
+    public abstract void setDateFormat(DateFormat df);
+    public abstract void setTsIndex(String path);
+    public abstract void close();
+
+}
