1ed432324544c842968b8a414a4aee23581517ea
craig-willis <willis8@illinois.edu>
2015-02-06 07:49:01 -0600

    Added Rutil class

---
+++ src/main/java/edu/gslis/temporal/util/RUtil.java
@@ -0,0 +1,49 @@
+package edu.gslis.temporal.util;
+
+import org.rosuda.REngine.Rserve.RConnection;
+
+
+public class RUtil {
+	
+
+	private RConnection c;
+
+	public RUtil() {
+		
+		try {
+			c = new RConnection();
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public double acf(double[] data) throws Exception { 
+	    return acf(data, 2);
+	}
+	public double acf(double[] data, int lag) throws Exception {
+        c.assign("x", data);
+        c.voidEval("ac <- acf(x, plot=F)");
+
+        return c.eval("ac$acf[" + lag + "]").asDouble();
+	}
+
+	public double kurtosis(double[] data) throws Exception {
+        c.assign("x", data);
+        c.voidEval("library(moments)");
+
+        c.voidEval("k <- kurtosis(x)");
+
+        return c.eval("k").asDouble();	    
+	}
+
+	public void close() {
+		try {
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
