97657b5d43650874451910409f684e56b1be213a
craig-willis <willis8@illinois.edu>
2015-02-07 20:45:42 -0600

    Removed dynamic smoothing, add-one smoothing of temporal model

--- src/main/java/edu/gslis/temporal/scorers/TSMScorer.java
+++ src/main/java/edu/gslis/temporal/scorers/TSMScorer.java
@@ -61,12 +61,23 @@ public class TSMScorer extends TemporalScorer
                 // Temporal model
                 double tfreq = tsIndex.get(feature, t);
                 double tlen = tsIndex.get("_total_", t);
-
                 
                 // Use Laplace smoothing for the temporal models. This is just for the case 
                 // where lambda = 1
-                double timePr = (tfreq + 1) / (tlen + collectionStats.getTokCount());                
+                //double timePr = (tfreq + 1) / (tlen + collectionStats.getTokCount()); 
+                double timePr = tfreq / tlen; 
                                 
+                // 2-stage smoothing
+                // 1. Smooth the document with the temporal model
+                double smoothedTemporalProb = 
+                        (docFreq + mu*timePr) / 
+                        (docLength + mu);
+                
+                // 2. Smooth the smoothed model with the collection model
+                double smoothedDocProb = 
+                        lambda*smoothedTemporalProb + (1-lambda)*collectionProb;
+
+                /*
                 // Smooth the temporal models with the collection model
                 double smoothedTemporalProb = 
                         lambda*timePr + (1-lambda)*collectionProb;
@@ -75,6 +86,7 @@ public class TSMScorer extends TemporalScorer
                 double smoothedDocProb = 
                         (docFreq + mu*smoothedTemporalProb) / 
                         (docLength + mu);
+                */
                 
                 double queryWeight = gQuery.getFeatureVector().getFeatureWeight(feature);
                 
@@ -98,109 +110,8 @@ public class TSMScorer extends TemporalScorer
             e.printStackTrace();
         }
     }
+    
     @Override
     public void init(SearchHits hits) {
-        
-
-        double lambda = paramTable.get(LAMBDA);
-
-        if (lambda == -1) {
-            
-            // use NPMI
-            if (gQuery.getFeatureVector().getFeatureCount() > 1)
-            {
-                // Dynamic smoothing, calculate correlation between NPMI values for all query terms
-                Iterator<String> queryIterator = gQuery.getFeatureVector().iterator();            
-                List<double[]> npmis = new ArrayList<double[]>();
-                List<String> terms = new ArrayList<String>();
-                while(queryIterator.hasNext()) 
-                {
-                    String feature = queryIterator.next();
-                    try {
-                        double[] npmi = tsIndex.getNpmi(feature);
-                        npmis.add(npmi);
-                        terms.add(feature);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-                PearsonsCorrelation cor = new PearsonsCorrelation();
-                double avgCor = 0;
-                int k = 0;
-                for (int i=0; i<npmis.size(); i++) {
-                    for (int j=i; j<npmis.size(); j++) {
-                        if (i==j) continue;
-//                        double[] npmii = npmis.get(i);
-//                        double[] npmij = npmis.get(j);
-
-                        double c = cor.correlation(npmis.get(i), npmis.get(j));
-                     //   System.out.println("\t cor: " + terms.get(i) + ", " + terms.get(j) + "=" + c);
-                       // for (int l=0; l<npmii.length; l++) {
-                       //     System.out.println(terms.get(i) + ", " + terms.get(j) 
-                       //             + "," + npmii[l]  + ", " + npmij[l] + "\n");
-                       // }
-                        
-                        avgCor += c;
-                        k++;
-                    }
-                }
-                avgCor /= k;
-                
-                if (avgCor > 0)     
-                    paramTable.put(LAMBDA, avgCor);
-                else 
-                    paramTable.put(LAMBDA, 0D); 
-
-            }
-            else
-                paramTable.put(LAMBDA, 0D);
-            
-            System.err.println(gQuery.getTitle() + " lambda=" + paramTable.get(LAMBDA));
-        }
-        else if (lambda == -2)         
-        {
-            // Dynamically set lambda using AC
-            if (gQuery.getFeatureVector().getFeatureCount() > 1)
-            {
-                try
-                {
-                    // Set lambda based on autocorrelation
-                    Map<Integer, Map<String, Double>> tms 
-                        = createTemporalModels(gQuery.getFeatureVector().getFeatures());
-    
-                    double[] scores = new double[tms.size()];
-                    double z = 0;            
-                    for (int bin: tms.keySet()) {
-                        // Log-likelihood of query given temporal model
-                        double ll = scoreTemporalModel(gQuery.getFeatureVector(), tms.get(bin));
-                        scores[bin] = Math.exp(ll);
-                        z += scores[bin];
-                    }
-            
-                    // p(theta_i given Q)
-                    for (int bin: tms.keySet()) {
-                        scores[bin] = scores[bin]/z;
-                    }
-                    
-                    // Calculate autocorrelation at lag 1
-                    RUtil rutil = new RUtil();
-                    double ac = rutil.acf(scores);
-                    if (ac > 0)     
-                        paramTable.put(LAMBDA, ac);
-                    else 
-                        paramTable.put(LAMBDA, 0D); 
-                    rutil.close();
-                    
-                    System.out.println(gQuery.getTitle() + "," + ac);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            else
-                paramTable.put(LAMBDA, 0D);
-            
-            System.err.println(gQuery.getTitle() + " lambda=" + paramTable.get(LAMBDA));            
-        }
-    }   
-    
+    }       
 }
