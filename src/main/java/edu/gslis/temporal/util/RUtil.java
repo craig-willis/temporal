7873ff43a3af41dda5b9363a3a9010cfeffe39b4
craig-willis <willis8@illinois.edu>
2015-03-10 13:41:00 -0500

    Added minima() method

--- src/main/java/edu/gslis/temporal/util/RUtil.java
+++ src/main/java/edu/gslis/temporal/util/RUtil.java
@@ -19,8 +19,50 @@ public class RUtil {
 		}
 	}
 	
+   public RUtil(int port) {
+        
+        try {
+            c = new RConnection("localhost", port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+   public int[] minima(int[] x, double[] y) throws Exception {
+       
+       c.assign("x", x);
+//       c.assign("y", y);
+       c.assign("weights", y);
+       c.voidEval("weights = weights / sum(weights)");
+       
+//       c.voidEval("y <- as.vector(y)");
+//       c.voidEval("dens <- density(y, bw=\"sj\")");
+       c.voidEval("dens <- density(x, weights=weights, bw=\"sj\")");
+       c.voidEval("second_deriv <- diff(sign(diff(dens$y)))");
+       c.voidEval("min <- which(second_deriv == 2) + 1");
+       
+       //c.voidEval("max <- which(second_deriv == -2) + 1");
+       c.voidEval("mp <- dens$x[min] / (max(dens$x) - min(dens$x))");
+       return c.eval("x[length(x)*mp]").asIntegers();
+   }
+   
+   
 	public double acf(double[] data) throws Exception { 
 	    return acf(data, 2);
+	}
+	
+	public double silvermantest(double[] data, int modes) throws Exception {
+	    c.voidEval("library(silvermantest)");
+        c.assign("x", data);
+        return c.eval("silverman.test(x, " + modes + ")@p_value").asDouble();	    	
+	}
+	
+	public double ccf(double[] x, double[]y, int lag) throws Exception  {
+        c.assign("x", x);
+        c.assign("y", y);
+        c.voidEval("cc <- ccf(x, y, plot=F)");
+
+        return c.eval("cc$acf[which(cc$lag == " + lag + ")]").asDouble();	    
 	}
 	public double acf(double[] data, int lag) throws Exception {
         c.assign("x", data);
