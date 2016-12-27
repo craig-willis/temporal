e2daafb3748f7759ff7a413eba72d99e21fd57a8
Craig Willis <willis8@illinois.edu>
2016-12-27 15:19:37 -0500

    Removed unused config options

--- src/main/java/edu/gslis/main/RunQuery.java
+++ src/main/java/edu/gslis/main/RunQuery.java
@@ -5,9 +5,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,31 +17,25 @@ import java.util.concurrent.TimeUnit;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
+import com.google.common.collect.Sets;
+
 import edu.gslis.config.BatchConfig;
 import edu.gslis.config.CollectionConfig;
 import edu.gslis.config.ScorerConfig;
 import edu.gslis.docscoring.support.CollectionStats;
-import edu.gslis.eval.Qrels;
 import edu.gslis.indexes.IndexWrapper;
 import edu.gslis.indexes.IndexWrapperFactory;
-import edu.gslis.indexes.temporal.TemporalLDAIndex;
-import edu.gslis.indexes.temporal.TimeSeriesIndex;
-import edu.gslis.indexes.temporal.lda.LDAIndex;
 import edu.gslis.lucene.indexer.Indexer;
 import edu.gslis.queries.GQueries;
 import edu.gslis.queries.GQueriesIndriImpl;
 import edu.gslis.queries.GQueriesJsonImpl;
 import edu.gslis.queries.GQuery;
-import edu.gslis.scorers.lda.LDAScorer;
-import edu.gslis.scorers.temporal.KDELDAScorer;
 import edu.gslis.scorers.temporal.RerankingScorer;
-import edu.gslis.scorers.temporal.TemporalLDAScorer;
 import edu.gslis.scorers.temporal.TemporalScorer;
-import edu.gslis.textrepresentation.FeatureVector;
-import ucar.unidata.util.StringUtil;
 
 
 /** 
+ * Multi-threaded query driver for YAMLConfigBase.
  * 
  * edu.gslis.temporal.main.RunQuery config/config.yaml
  * 
@@ -47,200 +43,153 @@ import ucar.unidata.util.StringUtil;
  *    For each configured scorer
  *       For each configured set of topics
  *          Run initial retrieval
- *          Rescore
- *          Calculate RM3 model
- *          Rescore
+ *          Re-score
  *          Output results
  */
 public class RunQuery extends YAMLConfigBase 
 {
-    public static final String NAME_OF_TIMESTAMP_FIELD = "timestamp";
     static final int NUM_THREADS = 10;
     
     public RunQuery(BatchConfig config) {
         super(config);
     }
 
-    public void runBatch(boolean rescoreRm3) throws Exception 
+    public void runBatch() throws Exception 
     {
         initGlobals();
         
+        
+        // For each collection
         List<CollectionConfig> collections = config.getCollections();
         for(CollectionConfig collection: collections) 
         {
             String collectionName = collection.getName();
-            System.out.println("Processing " + collectionName);
-
-            String timeSeriesDBPath = collection.getTsDB();
-            String ldaTermTopicPath = collection.getLdaTermTopicPath();
-            String ldaDocTopicsPath = collection.getLdaDocTopicsPath();
-            String ldaDataPath = collection.getLdaPath();
-            
-            String qrelPath = collection.getTrainQrels();
-            Qrels qrels = new Qrels(qrelPath, true, collection.getRelLevel());
+            System.err.println("Processing " + collectionName);
             
             long startTime = collection.getStartDate();
             long endTime = collection.getEndDate();
-            long interval = collection.getInterval();
+            
+            // For each query set
             Map<String, String> queryFiles = collection.getQueries();
             for (String queryFileName: queryFiles.keySet()) {
                 System.err.println(collectionName + " " + queryFileName);
                 String queryFilePath = queryFiles.get(queryFileName);
                 
+                // Read the queries
                 GQueries queries = null;
                 if (queryFilePath.endsWith("indri")) 
                     queries = new GQueriesIndriImpl();
                 else
                     queries = new GQueriesJsonImpl();
-
                 
-                queries.setMetadataField(NAME_OF_TIMESTAMP_FIELD);
                 queries.read(queryFilePath);
                 
-                String indexPath = indexRoot + File.separator + collection.getTestIndex();
+                // Open the index
+                String indexPath = indexRoot + File.separator + collection.getIndex();
                 IndexWrapper index = 
                         IndexWrapperFactory.getIndexWrapper(indexPath);
                 index.setTimeFieldName(Indexer.FIELD_EPOCH);
-                
-                // Calculate KL(CM||TM) for each temporal model
-                TimeSeriesIndex timeSeriesIndex = new TimeSeriesIndex();
-                int numBins = 0;
-                if (interval > 0) 
-                    numBins = (int) ((endTime - startTime) / interval);
-                double[] klweights = new double[numBins];
-                if (StringUtil.notEmpty(timeSeriesDBPath)) {
-                    timeSeriesIndex.open(timeSeriesDBPath, true, "csv");
-                    
-                    /*
-                    System.out.println("Calculating KL divergence");
-                    // Calculate KL divergence
-                    TimeSeriesKL tskl = new TimeSeriesKL();
-                    klweights = tskl.calculateBinKL(index, timeSeriesIndex);
-                                       
-                    System.out.println("done");
-                    */
-                }
-                
-                LDAIndex ldaIndex = new LDAIndex();
-                if (StringUtil.notEmpty(ldaDocTopicsPath) &&
-                        StringUtil.notEmpty(ldaTermTopicPath))   
-                {             
-                    //ldaIndex.open(ldaDBPath, true);
-                    System.out.println("Loading LDA data");
-                    ldaIndex.load(ldaDocTopicsPath, ldaTermTopicPath);
-                    System.out.println("done");
-                }
-                
-                TemporalLDAIndex tempLdaIndex = new TemporalLDAIndex();
-                if (StringUtil.notEmpty(ldaDataPath)) {
-                    System.out.println("Loading temporal LDA data");
-                    tempLdaIndex.load(ldaDataPath);
-                    System.out.println("done");
-                }
-                                    
+                                                    
                 String corpusStatsClass = config.getBgStatType();
                 CollectionStats corpusStats = (CollectionStats)loader.loadClass(corpusStatsClass).newInstance();
                 corpusStats.setStatSource(indexPath);
-                                    
-                
+                             
+                // For each scorer
                 List<ScorerConfig> scorerConfigs = config.getScorers();
                 for (ScorerConfig scorerConfig: scorerConfigs) 
                 {               
                     // Setup the scorers
                     String scorerName = scorerConfig.getName();
                     String className = scorerConfig.getClassName();
-                    System.out.println("Running scorer " + scorerName);
+                    System.err.println("Running scorer " + scorerName);
+                    
+                    // Get all permutations of configured parameters
+                    Map<String, String> paramMap = scorerConfig.getParams();
+                    List<Set<Double>> plist = new ArrayList<Set<Double>>();
+                    List<String> paramNames = new ArrayList<String>();
+                    for (String param: paramMap.keySet()) {
+                    	paramNames.add(param);
+                    	
+                    	String paramStr = paramMap.get(param);
+                    	String[] paramElems = paramStr.split(",");
+                    	Double[] params = new Double[paramElems.length];
+                    	for (int i=0; i<paramElems.length; i++) {
+                    		params[i] = Double.valueOf(paramElems[i]);
+                    	}
+                    	plist.add(Sets.newHashSet(params));
+                    }
+                    
+                    Set<List<Double>> prod = Sets.cartesianProduct(plist);
 
-                    
-                    String runId = prefix + "-" + scorerName + "_" + collectionName + "_" + queryFileName;
-                    String trecResultsFile = outputDir + File.separator + runId + ".out";
-                    
-                    String rm3Config = scorerConfig.getLambda() + ":" + scorerConfig.getNumFeedbackDocs() + ":" + scorerConfig.getNumFeedbackTerms();
-                    String runIdRm3 = prefix + "-rm3:" + rm3Config + "-"+ scorerName + "_" + collectionName + "_" + queryFileName;
-                    String trecResultsFileRm3 = outputDir + File.separator + runIdRm3 + ".out";
-                    
-                    outputDir.mkdirs();                        
-
-                    Writer trecResultsWriter = new BufferedWriter(new FileWriter(trecResultsFile));
-                    FormattedOutputTrecEval trecFormattedWriter = new FormattedOutputTrecEval();
-                    trecFormattedWriter.setRunId(runId);
-                    trecFormattedWriter.setWriter(trecResultsWriter);
-
-                    Writer trecResultsWriterRm3 = new BufferedWriter(new FileWriter(trecResultsFileRm3));
-                    FormattedOutputTrecEval trecFormattedWriterRm3 = new FormattedOutputTrecEval();
-                    trecFormattedWriterRm3.setRunId(runIdRm3);
-                    trecFormattedWriterRm3.setWriter(trecResultsWriterRm3);
-                    
-                    int numThreads = config.getNumThreads();
-                    ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-                    Iterator<GQuery> queryIterator = queries.iterator();
-                    while(queryIterator.hasNext()) 
-                    {                            
-                        GQuery query = queryIterator.next();
-                                                
+                    // for each parameter combination
+                    for (List<Double> params: prod) {
+                    	
                         // Scorer per worker means scorer per query
                         RerankingScorer docScorer = (RerankingScorer)loader.loadClass(className).newInstance();
                         
                         docScorer.setConfig(scorerConfig);
                         docScorer.setCollectionStats(corpusStats);
                         
-                        Map<String, Object> params = scorerConfig.getParams();
-                        for (String paramName: params.keySet()) {
-                            Object obj = params.get(paramName);
-                            if (obj instanceof Double) { 
-                                docScorer.setParameter(paramName, (Double)obj);
-                            }
-                            else if (obj instanceof Integer) { 
-                                docScorer.setParameter(paramName, (Integer)obj);
-                            }
-                            else if (obj instanceof String) {
-                                docScorer.setParameter(paramName, (String)obj);
-                            }
-                        }
-                        docScorer.init();
-                
                         if (docScorer instanceof TemporalScorer) {
-                            ((TemporalScorer)docScorer).setIndex(timeSeriesIndex);
-                            ((TemporalScorer)docScorer).setStartTime(startTime);
-                            ((TemporalScorer)docScorer).setEndTime(endTime);
-                            ((TemporalScorer)docScorer).setInterval(interval);
-                            ((TemporalScorer)docScorer).setKLs(klweights);
+                        	((TemporalScorer)docScorer).setStartTime(startTime);
+                        	((TemporalScorer)docScorer).setStartTime(endTime);
                         }
-                        if (docScorer instanceof LDAScorer)
-                            ((LDAScorer)docScorer).setIndex(ldaIndex);
-                        if (docScorer instanceof KDELDAScorer)
-                            ((KDELDAScorer)docScorer).setIndex(ldaIndex);
-                        if (docScorer instanceof TemporalLDAScorer)
-                            ((TemporalLDAScorer)docScorer).setIndex(tempLdaIndex);
                         
+                    	String paramStr = "";
+                    	for (int i=0; i<params.size(); i++) {
+                        	String name = paramNames.get(i);
+                        	Double value = params.get(i);
+                            docScorer.setParameter(name, value);
+                            if (paramStr.length() > 0) 
+                            	paramStr += ":";
+                            paramStr += name +"=" + value;
+                    	}
+                    	                    	                   
+                    	// Begin: Multi-threaded query runner
+                    	
+                        // Create output file
+                    	File resultsDir = new File(outputDir + "/" + collectionName + "/" + queryFileName + "/" + scorerName);
+                    	resultsDir.mkdirs();     
+                        String runId = scorerName + "_" + paramStr;
+                        String trecResultsFile = resultsDir + File.separator + paramStr + ".out";
                         
-                        QueryRunner worker = new QueryRunner();
-                        worker.setDocScorer(docScorer);
-                        worker.setIndex(index);
-                        worker.setQrels(qrels);
-                        worker.setQuery(query);
-                        worker.setStopper(stopper);
-                        worker.setTrecFormattedWriter(trecFormattedWriter);
-                        worker.setTrecFormattedWriterRm3(trecFormattedWriterRm3);
-                        worker.setCollectionStats(corpusStats);
-                        worker.setNumFeedbackDocs(scorerConfig.getNumFeedbackDocsArray()[0]);
-                        worker.setNumFeedbackTerms(scorerConfig.getNumFeedbackTermsArray()[0]);
-                        worker.setRmLambda(scorerConfig.getLambdaArray()[0]);
-                        worker.setRescoreRm3(rescoreRm3);
-                        executor.execute(worker);
-                    }
-                    
-                    executor.shutdown();
-                    // Wait until all threads are finish
-                    executor.awaitTermination(360, TimeUnit.MINUTES);
-                    System.out.println("Finished all threads");
-                    
-                    trecFormattedWriter.close();
-                    trecFormattedWriterRm3.close();
+                        Writer trecResultsWriter = new BufferedWriter(new FileWriter(trecResultsFile));
+                        FormattedOutputTrecEval trecFormattedWriter = new FormattedOutputTrecEval();
+                        trecFormattedWriter.setRunId(runId);
+                        trecFormattedWriter.setWriter(trecResultsWriter);
+                                         
+                        // Start a thread per query
+                        int numThreads = config.getNumThreads();
+                        if (numThreads == 0) { numThreads = 1; }
+                        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
+                        Iterator<GQuery> queryIterator = queries.iterator();
+                        while(queryIterator.hasNext()) 
+                        {                            
+	                        GQuery query = queryIterator.next();
+	                        docScorer.setQuery(query);		                                                
+	                        docScorer.init();
+	                                
+	                        // Run worker
+	                        QueryRunner worker = new QueryRunner();
+	                        worker.setDocScorer(docScorer);
+	                        worker.setIndex(index);
+	                        worker.setQuery(query);
+	                        worker.setStopper(stopper);
+	                        worker.setTrecFormattedWriter(trecFormattedWriter);
+	                        worker.setCollectionStats(corpusStats);
+	                        executor.execute(worker);
+	                    }
+                        
+                        executor.shutdown();
+                        // Wait until all threads are finish
+                        executor.awaitTermination(360, TimeUnit.MINUTES);
+                        System.err.println("Finished all threads");
+                        
+                        trecFormattedWriter.close();
+                        // End multi-threaded query runner
+                    }
                 }
-                
-                ldaIndex.close();
             }
         }    
     }
@@ -253,30 +202,12 @@ public class RunQuery extends YAMLConfigBase
             System.exit(-1);
         }
         
-        boolean rescoreRm3 = true;
-        if (args.length == 2) {
-            rescoreRm3 = Boolean.parseBoolean(args[1]);
-        }
-
         // Read the yaml config
         Yaml yaml = new Yaml(new Constructor(BatchConfig.class));
         BatchConfig config = (BatchConfig)yaml.load(new FileInputStream(yamlFile));
 
         RunQuery runner = new RunQuery(config);
-        runner.runBatch(rescoreRm3);
+        runner.runBatch();
     }
-    
-    public static FeatureVector cleanModel(FeatureVector model) {
-        FeatureVector cleaned = new FeatureVector(null);
-        Iterator<String> it = model.iterator();
-        while(it.hasNext()) {
-            String term = it.next();
-            if(term.length() < 3 || term.matches(".*[0-9].*"))
-                continue;
-            cleaned.addTerm(term, model.getFeatureWeight(term));
-        }
-        cleaned.normalize();
-        return cleaned;
-    }
-    
+       
 }
